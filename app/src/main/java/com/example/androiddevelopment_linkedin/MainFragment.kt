7e79c4c3b9a071fdbf99569909abeea0eec2fbd4
Michael808000 <michael808000@gmail.com>
2021-10-11 22:56:06 +0800

    Upload the initial project to git

---
+++ app/src/main/java/com/example/androiddevelopment_linkedin/MainFragment.kt
@@ -0,0 +1,33 @@
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.androiddevelopment_linkedin.R
+import com.example.androiddevelopment_linkedin.MainViewModel
+
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.activity_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
