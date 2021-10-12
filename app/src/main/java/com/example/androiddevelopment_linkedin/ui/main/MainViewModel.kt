15ce42cbb6caebe8a4ab1031b91527808ac35571
Michael808000 <michael808000@gmail.com>
2021-10-12 19:53:07 +0800

    initial commit

--- app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.example.androiddevelopment_linkedin.LOG_TAG
 import com.example.androiddevelopment_linkedin.data.Monster
+import com.example.androiddevelopment_linkedin.data.MonsterRepository
 import com.example.androiddevelopment_linkedin.utilies.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -12,23 +13,15 @@ import com.squareup.moshi.Types
 
 class MainViewModel(app:Application):AndroidViewModel(app) {
 
-    private val listType = Types.newParameterizedType(
-        List::class.java, Monster::class.java)
+    private val dataRepo =MonsterRepository()
 
     init {
 //        val text1 = FileHelper.getTextFromResources(app,R.raw.monster_data)
 //        Log.i(LOG_TAG,text1)
-        val text = FileHelper.getTextFromAssets(app,"monster_data.json")
-       parseText(text)
-    }
-    fun parseText(text:String){
-        val moshi = Moshi.Builder().build()
-        val adapter: JsonAdapter<List<Monster>> =moshi.adapter(listType)
-        val  monsterData = adapter.fromJson(text)
-        for(monster in monsterData?: emptyList()){
+        val monsterData =dataRepo.getMonsterData(app)
+        for(monster in monsterData){
             Log.i(LOG_TAG, "${monster.monsterName} (\$${monster.price})")
         }
-
     }
 
 }
