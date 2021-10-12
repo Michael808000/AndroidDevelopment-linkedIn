fcc9ff01f528e8a1b8d02c096632dd4a86e09799
Michael808000 <michael808000@gmail.com>
2021-10-12 12:41:25 +0800

    update the layout

--- app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.androiddevelopment_linkedin.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.example.androiddevelopment_linkedin.R
+import com.example.androiddevelopment_linkedin.utilies.FileHelper
 
-class MainViewModel: ViewModel() {
+class MainViewModel(app:Application):AndroidViewModel(app) {
+    init {
+        val text = FileHelper.getTextFromResources(app,R.raw.monster_data)
+        Log.i("monsterLogging",text.toString())
+    }
 
 }
