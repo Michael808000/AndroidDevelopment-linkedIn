546d4f908dc2ab3c17d9b18c2bd6071bd46e455b
Michael808000 <michael808000@gmail.com>
2021-10-12 21:48:36 +0800

    Share data with Livedata objects

--- app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainFragment.kt
@@ -1,20 +1,19 @@
 package com.example.androiddevelopment_linkedin.ui.main
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.androiddevelopment_linkedin.R
-import com.example.androiddevelopment_linkedin.data.Monster
+import java.lang.StringBuilder
+import kotlinx.android.synthetic.main.fragment_main.message
+
+
 
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
 
     private lateinit var viewModel: MainViewModel
 
@@ -22,16 +21,19 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val monster = Monster(monsterName = "Bob",imageFile = "myFile",caption = "a caption",description = "Some description",price = 0.19, scariness = 3)
-        //Log.i("monsterLogging", monster.toString())
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.monsterData.observe(viewLifecycleOwner, Observer {
+            val monsterNames = StringBuilder()
+            for(monster in it){
+//                Log.i(LOG_TAG, "${monster.monsterName} (\$${monster.price})")
+                monsterNames.append(monster.monsterName).append("\n")
+            }
+            message.text=monsterNames
+        })
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
