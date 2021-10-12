f43cef4b236e0e987c22890d58f7f2fff70a3e5d
Michael808000 <michael808000@gmail.com>
2021-10-12 11:08:39 +0800

    update the layout

--- app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/androiddevelopment_linkedin/ui/main/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.example.androiddevelopment_linkedin.ui.main
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.androiddevelopment_linkedin.R
+import com.example.androiddevelopment_linkedin.data.Monster
 
 
 class MainFragment : Fragment() {
@@ -20,9 +22,11 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        val monster = Monster(monsterName = "Bob",imageFile = "myFile",caption = "a caption",description = "Some description",price = 0.19, scariness = 3)
+        Log.i("monsterLogging", monster.toString())
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
